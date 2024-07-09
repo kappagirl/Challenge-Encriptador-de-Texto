f38a89553e6fd2bfac1bc0204d1fc98c59053882
kappagirl <barbosa.cardenas.danna.marilyn@gmail.com>
2024-07-08 21:29:02 -0500

    Version final

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Challenge-Encriptador-de-Texto
+
+El siguiente código es de una aplicación que encripta textos para intercambiar mensajes secretos con otras personas que sepan el secreto de la encriptación utilizada.
+
+## Llaves de Encriptación
+
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+- La letra "e" es convertida para "enter"
+- La letra "i" es convertida para "imes"
+- La letra "a" es convertida para "ai"
+- La letra "o" es convertida para "ober"
+- La letra "u" es convertida para "ufat"
+
+## Características
+
+- Solo funciona con letras minúsculas.
+- No funciona con caracteres especiales ni letras con acentos.
+- Funciona para convertir una palabra a su versión encriptada y también devolver una palabra encriptada a su versión original.
+
+### Ejemplos
+
+- "gato" => "gaitober"
+- "gaitober" => "gato"
+
+## Funcionalidades de la Página
+
+La página tiene campos para:
+
+1. Insertar el texto que será encriptado o desencriptado, y el usuario puede escoger entre las dos opciones.
+2. Mostrar el resultado en la pantalla.
+
+### Extras
+
+- Adicionalmente la página tiene un botón para copiar el texto encriptado/desencriptado
+
+!Hagamos encriptación y desencriptación de mensajes!
--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-let texto
+const esconderBotonCopiar = document.querySelector('.botonExtra');
+esconderBotonCopiar.style.display = 'none';
+
+const miTextarea = document.getElementById('salida');
+miTextarea.style.display = 'none';
+
+const esconderImagen = document.querySelector('.contenedorImagen');
+const esconderAlertas = document.querySelector('.alertas');
+
+function encriptacion() {
+
+    esconderBotonCopiar.style.display = 'flex';
+    miTextarea.style.display = 'block';
+    esconderImagen.style.display = 'none';
+    esconderAlertas.style.display = 'none';
+
+    let texto = document.getElementById("entrada").value;
+    let encriptar = texto.replace(/e/g, "enter")
+                            .replace(/i/g, "imes")
+                            .replace(/a/g, "ai")
+                            .replace(/o/g, "ober")
+                            .replace(/u/g, "ufat");
+    
+    document.getElementById("salida").innerText = encriptar;
+
+}
+
+function desencriptacion () {
+
+    esconderBotonCopiar.style.display = 'flex';
+    miTextarea.style.display = 'block';
+    esconderImagen.style.display = 'none';
+    esconderAlertas.style.display = 'none';
+    
+    let textoIngresado = document.getElementById("entrada").value;
+    let desencriptar = textoIngresado.replace(/enter/g, "e")
+                                        .replace(/imes/g, "i")
+                                        .replace(/ai/g, "a")
+                                        .replace(/ober/g, "o")
+                                        .replace(/ufat/g, "u");
+    
+    document.getElementById("salida").innerHTML = desencriptar;
+
+}
+
+function copiar () {
+    let copiarTexto = document.getElementById("salida");
+    copiarTexto.select();
+
+    document.execCommand('copy');
+}
